6765c080e86c327b5bfa8600c8fee26d01587528
omgjoon <surinegann@gmail.com>
2023-06-15 17:46:41 -0300

    atualizando

---
+++ bancoso/src/index.js
@@ -0,0 +1,18 @@
+import 'express-async-errors';
+import express from 'express';
+import morgan from 'morgan';
+import router from './router.js';
+
+const server = express ();
+
+server.use(morgan('tiny'));
+
+server.use(express.json());
+
+server.use(express.static('public'));
+
+server.use(router);
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
