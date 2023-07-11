5e749589ae7b48fd9639e03b87645e1a82940ced
omgjoon <surinegann@gmail.com>
2023-07-11 19:52:00 -0300

    view posts in front: ok

--- public/front-js/post.js
+++ public/front-js/post.js
@@ -1,9 +1,26 @@
-//olha os codigos de arthur pra fazer a postagem aparecer, dessa vez vai dar certo, vambora vambora
+function genPostagem (postagem) {
+    const html = `
+    <h1>${postagem.titulo}</h1>
+    <p>${postagem.conteudo}</p>`
+
+    return html
+}
+
+function insertPost (postagem) {
+    const post = document.querySelector('.grid-posts')
+    const postagemView = genPostagem(postagem)
+
+    post.insertAdjacentHTML('beforeend', postagemView)
+}
 
 async function showPostagens () {
     const postagens = await fetch('/posts').then(res => res.json())
 
     console.log(postagens)
+
+    postagens.forEach(element => {
+        insertPost(element)
+    })
 }
 
-showPostagens()
+showPostagens ()
