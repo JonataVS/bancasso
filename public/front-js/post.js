9aa43d2f67950ed5b70eda7f2054a309ee7adfea
omgjoon <surinegann@gmail.com>
2023-08-05 23:36:41 -0300

    up/del post finish

--- public/front-js/post.js
+++ public/front-js/post.js
@@ -1,56 +1,54 @@
+// genPostagem contains the HTML of the post
+
 function genPostagem (postagem) {
     const html = `
     <div class="postagem" id="Cod_Post-${postagem.Cod_Post}">
-    <button type="submit" class="delete">Deletar</button>
     <h1>${postagem.titulo}</h1>
     <p>${postagem.conteudo}</p>
-    </div><br>
-    <div class="formulario" id="Cod_Post-${postagem.Cod_Post}">
+    <br>
     <form action="/posts/${postagem.Cod_Post}" method="POST">
     <label for="titulo">Título da Postagem</label><br>
     <input type="text" name="titulo" maxlength="500" required><br>
     <label for="conteudo">Conteúdo:</label><br>
     <textarea name="conteudo" required></textarea><br>
     <input type="submit" value="Editar"><br>
-    </form>
-</div><br>
+    </form><br>
+    <button type="submit" class="delete" value="Cod_Post-${postagem.Cod_Post}">Deletar</button>
+</div>
     `
     return html
 }
 
+// insertPost insert post in body HTML
+
 function insertPost (postagem) {
-    const post = document.querySelector('.grid-posts')
+    const gridPost = document.querySelector('.grid-posts')
     const postagemView = genPostagem(postagem)
 
-    post.insertAdjacentHTML('beforeend', postagemView)
-    const deleteButton = post.querySelector('.delete')
+    gridPost.insertAdjacentHTML('beforeend', postagemView)
+
+// function button for remove post     
+
+    const localPost = gridPost.querySelector(`#Cod_Post-${postagem.Cod_Post}`)
+    
+    const deleteButton = localPost.querySelector('.delete')
 
     deleteButton.onclick = () => {
         fetch(`/posts/${postagem.Cod_Post}`, {method: 'DELETE'});
 
-        post.remove();
+        localPost.remove();
  }
-    /*function addDeleteButton (postagem) {
-        const post = document.body.querySelector(`#Cod_Post-${postagem.Cod_Post}`)
-        const deleteButton = post.querySelector('.delete')
-    
-        deleteButton.onclick = () => {
-            fetch(`/posts/${postagem.Cod_Post}`, {method: 'DELETE'});
-    
-            post.remove();
-     }
-    }*/
-  
 }
+
+// showPostagens return posts in HTML
 
 async function showPostagens () {
     const postagens = await fetch('/getposts').then(res => res.json())
 
     console.log(postagens)
 
-    postagens.forEach(element => {
-        insertPost(element)
-    })
+    postagens.forEach(element => insertPost(element))
+
 }
 
 showPostagens ()
