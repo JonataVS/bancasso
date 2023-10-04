c2383fe64f4cf075675e215b1b07dbbb42a2bf2d
omgjoon <surinegann@gmail.com>
2023-10-04 20:50:42 -0300

    fix: validation

--- public/front-js/cadastro.js
+++ public/front-js/cadastro.js
@@ -2,18 +2,26 @@ window.handleSubmit = handleSubmit;
 
 const form = document.querySelector('form');
 
-const senha = form.senha.value
+// esses são as const e as validações de cada input 
 
+const senhaInput = form.senha;
+const senhaFeedback = document.querySelector('#senha + .invalid-feedback');
+
+const emailInput = form.email;
+const emailFeedback = document.querySelector('#email + .invalid-feedback');
+
+const nomeInput = form.nome;
+const nomeFeedback = document.querySelector('#nome + .invalid-feedback');
 
 
 async function handleSubmit(event) {
   event.preventDefault();
+  checkForm()
 
   if (form.checkValidity()) {
     const Usuario = Object.fromEntries(new FormData(form));
 
     //console.log(usuario);
-
 
     const config = {
       method: 'post',
@@ -23,7 +31,7 @@ async function handleSubmit(event) {
       },
     };
 
-    console.log(config);    
+    console.log(config);
 
     const response = await fetch('/usuario', config);
 
@@ -34,15 +42,66 @@ async function handleSubmit(event) {
     } else {
       console.log('Error no cadastro');
     }
-  } else {
-    if (form.nome.value === "") {
-      document.querySelector('#nome + .invalid-feedback').style.display = 'block'
-    } 
-    if (form.email.value === "") {
-      document.querySelector('#email + .invalid-feedback').style.display = 'block'
-    } 
-     if (senha < 8) {
-      document.querySelector('#senha + .invalid-feedback').style.display = 'block';
-    }
   }
+
 };
+
+// função para escutar cada input e excutar a função que está dentro dele
+
+nomeInput.addEventListener('input', function () {
+  checkname()
+})
+
+emailInput.addEventListener('input', function () {
+  checkemail()
+})
+
+senhaInput.addEventListener('input', function () {
+  checksenha()
+})
+
+
+function checkForm(){
+  checkemail()
+  checkname()
+  checksenha()
+}
+
+
+
+// validação de nome
+
+function checkname() {
+  const nome = nomeInput.value
+
+  if (nome === "") {
+    nomeFeedback.style.visibility = 'visible';
+  } else {
+    nomeFeedback.style.visibility = 'hidden';
+  }
+}
+
+// validacão de email
+
+function checkemail() {
+  const email = emailInput.value;
+
+  if (email === "") {
+    emailFeedback.style.visibility = 'visible';
+  } else {
+    emailFeedback.style.visibility = 'hidden';
+  }
+}
+
+// validação de senha
+
+function checksenha() {
+  const senha = senhaInput.value;
+
+  if (senha.length < 8) {
+    senhaFeedback.style.visibility = 'visible';
+  } else {
+    senhaFeedback.style.visibility = 'hidden';
+  }
+}
+
